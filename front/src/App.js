44a0f1e1e2783d102bf8d827c4a40c74fbe93730
Yafet Suleman <yafet.suleman.ys@gmail.com>
2023-05-25 16:11:16 +0200

    wip

--- front/src/App.js
+++ front/src/App.js
@@ -1,36 +1,38 @@
 import "./App.css";
-import React, {useState} from "react";
-import { GoogleMap, InfoWindow, Marker, useJsApiLoader } from "@react-google-maps/api";
-
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+    GoogleMap,
+    InfoWindow,
+    // Marker,
+    useJsApiLoader,
+} from "@react-google-maps/api";
+import Navbar from "./Navbar";
+import Entrieslist from "./entrieslist";
 
 const containerStyle = {
     width: "100vw",
     height: "96vh",
 };
 
-const zoom = 15;
+const zoom = 12;
 const center = {
     lat: 43.295619380624494,
     lng: 5.373698165000376,
 };
 
-
-
-
-
 function App() {
+    // fetch("/api/entries", {
+    //     method: "GET",
+    //     headers: { "Content-type": "application/json" },
+    // })
+    //     .then((result) => {
+    //         return result.json();
+    //     })
+    //     .then((data) => console.log(data));
 
-    fetch("/api/entries", {
-        method: "GET",
-        headers: { "Content-type": "application/json" },
-    })
-        .then((result) => {
-            return result.json();
-        })
-        .then((data) => console.log(data));
-
-    const [option, setOption] = useState('');
-    const [description, setDescription] = useState('');
+    const [option, setOption] = useState("");
+    const [description, setDescription] = useState("");
 
     const { isLoaded } = useJsApiLoader({
         id: "google-map-script",
@@ -41,75 +43,84 @@ function App() {
 
     const handleMapClick = (event) => {
         const clickedPosition = {
-          lat: event.latLng.lat(),
-          lng: event.latLng.lng(),
+            lat: event.latLng.lat(),
+            lng: event.latLng.lng(),
         };
         setInfoWindow(clickedPosition);
     };
 
-        const handleSubmit = event => {
-            event.preventDefault();
-            console.log(option);
-            console.log(description);
-        }
-
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(option);
+        console.log(description);
+    };
 
     return isLoaded ? (
         <div className="App">
-            <header>
-
-            </header>   
-                <nav className ="navbar">
-                    <li>
-                    <a href="/">Main</a>
-                    </li>
-                    <li>
-                    <a href="/">Latest Entries</a>
-                    </li>
-                    <li>
-                    <a href="/">The Team</a>
-                    </li>
-                </nav>
-                    <GoogleMap
-                    mapContainerStyle={containerStyle}
-                    center={center}
-                    zoom={zoom}
-                    onClick={handleMapClick}
-
-                    // onLoad={onLoad}
-                    // onUnmount={onUnmount}
-                >
-                    {infoWindow && (
+            <Router>
+                {/* <div className="Navbar"> */}
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<App />} />
+                    <Route path="/entries" element={<Entrieslist />} />
+                </Routes>
+                {/* </div> */}
+            </Router>
+            <GoogleMap
+                mapContainerStyle={containerStyle}
+                center={center}
+                zoom={zoom}
+                onClick={handleMapClick}
+            >
+                {infoWindow && (
                     <InfoWindow position={infoWindow}>
-                    <form >     
-                    {/* onSubmit={handleSubmit} */}
-                    <div>New Entry at  ({infoWindow.lat}/{infoWindow.lng})</div><pre></pre>
-                    <label>
-                        What have you noticed?: <pre></pre>
                         <form>
-                        <select value = {option}>
-                            <option value="lights">Lights Broken</option>
-                            <option value="traffic">Messy Traffic</option>
-                            <option value="pot">Potholes</option>
-                            <option value="construct">Dirty / Unaccessible Construction Zone</option>
-                            <option value="sidewalk">Small Sidewalk</option>
-                        </select>
+                            {/* onSubmit={handleSubmit} */}
+                            <div>
+                                New Entry at ({infoWindow.lat}/{infoWindow.lng})
+                            </div>
+                            <pre></pre>
+                            <label>
+                                What have you noticed?: <pre></pre>
+                                <form>
+                                    <select value={option}>
+                                        <option value="lights">
+                                            Lights Broken
+                                        </option>
+                                        <option value="traffic">
+                                            Messy Traffic
+                                        </option>
+                                        <option value="pot">Potholes</option>
+                                        <option value="construct">
+                                            Dirty / Unaccessible Construction
+                                            Zone
+                                        </option>
+                                        <option value="sidewalk">
+                                            Small Sidewalk
+                                        </option>
+                                    </select>
+                                </form>
+                            </label>
+                            <pre></pre>
+                            <label>
+                                Briefly explain the issue: <pre></pre>
+                                <input
+                                    className="descriptionfield"
+                                    type="text"
+                                    maxLength="100"
+                                    value={description}
+                                />
+                            </label>{" "}
+                            <pre></pre>
+                            <input type="submit" value="Submit" />
                         </form>
-                    </label>
-                    <pre></pre>
-                    <label>
-                        Briefly explain the issue: <pre></pre>
-                        <input className="descriptionfield" type="text" maxLength="100" value={description} />
-                    </label> <pre></pre>
-                    <input type="submit" value="Submit"  />
-                    </form>
                     </InfoWindow>
-                    )}
-                </GoogleMap>
-        </div>):("Loading...")
-    
+                )}
+            </GoogleMap>
+        </div>
+    ) : (
+        "Loading..."
+    );
 }
 
 export default App;
-
-
--- front/src/Navbar.jsx
+++ front/src/Navbar.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function Navbar() {
-  return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/blogs">Blogs</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-      </ul>
-    </nav>
-  );
+class Navbar extends React.Component {
+    render() {
+        return (
+            <nav>
+                <h1 className="navBar">Navbar</h1>
+                <ul class="rowList">
+                    <li>
+                        <Link to="/">Main</Link>
+                    </li>
+                    <li>
+                        <Link to="/entries">Latest entries</Link>
+                    </li>
+                    <li>
+                        <Link to="/team">The team</Link>
+                    </li>
+                </ul>
+            </nav>
+        );
+    }
 }
 
 export default Navbar;
