6b4abe3bc75f1b727f5d3f77095703a7fb77f54f
TilliRS <pptry@mail.de>
2023-05-25 14:25:30 +0200

    new

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
-import React from "react";
-import { GoogleMap, MarkerF, useJsApiLoader } from "@react-google-maps/api";
+import React, {useState} from "react";
+import { GoogleMap, InfoWindow, Marker, useJsApiLoader } from "@react-google-maps/api";
+
 
 const containerStyle = {
-    width: "60vw",
-    height: "60vh",
+    width: "100vw",
+    height: "96vh",
 };
 
 const zoom = 15;
@@ -13,69 +14,118 @@ const center = {
     lng: 5.373698165000376,
 };
 
-const places = [
-    {
-        key: "Cool",
-        name: "La Friche",
-        coord: { lat: 43.31064201725967, lng: 5.39046901171601 },
-        icon: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
-    },
-    {
-        key: "Booo",
-        name: "Polytech Luminy",
-        coord: { lat: 43.23207174121906, lng: 5.4431462110285045 },
-        icon: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
-    },
-];
+
+
+
 
 function App() {
+    const [option, setOption] = useState('');
+    const [description, setDescription] = useState('');
+
     const { isLoaded } = useJsApiLoader({
         id: "google-map-script",
         googleMapsApiKey: "AIzaSyCDB95dhVP6ZyBgqoIMW1LCGEFWcs_k_EM",
     });
 
-    // const [map, setMap] = React.useState(null);
+    const [infoWindow, setInfoWindow] = useState(null);
 
-    // const onLoad = React.useCallback(function callback(map) {
-    //     // This is just an example of getting and using the map instance!!! don't just blindly copy!
-    //     // map.setZoom(zoom);
+    const handleMapClick = (event) => {
+        const clickedPosition = {
+          lat: event.latLng.lat(),
+          lng: event.latLng.lng(),
+        };
+        setInfoWindow(clickedPosition);
+    };
 
-    //     setMap(map);
-    // }, []);
+        // // Adds a new Entry when Submitting
+        // const handleSubmit = event => {
+        //     event.preventDefault(); // no reloading of page
+        //     const newEntry = {
+        //       id:   0,
+        //       type: type,
+        //       description: description
+        //     };
+        //     fetch('http://localhost:4000/api', {
+        //       method: 'POST',
+        //       headers: {
+        //         'Content-Type': 'application/json'
+        //       },
+        //       body: JSON.stringify(newEntry)
+        //     })
+        //       .then(response => response.json())
+              
+        //       .catch(error => {
+        //         console.log(error)
+        //         alert('An error occurred while adding the user. Please try again later.');
+        //       }); 
+        //   };
 
-    // const onUnmount = React.useCallback(function callback(map) {
-    //     setMap(null);
-    // }, []);
+        const handleSubmit = event => {
+            event.preventDefault();
+            console.log(option);
+            console.log(description);
+        }
+
 
     return isLoaded ? (
-        <GoogleMap
-            mapContainerStyle={containerStyle}
-            center={center}
-            zoom={zoom}
-            // onLoad={onLoad}
-            // onUnmount={onUnmount}
-        >
-            {
-                /* Child components, such as markers, info windows, etc. */
-                /* We should create the objects in defined functions, not
-                map them in the return. Just google how, or we can discuss
-                together. */
-                places.map((place) => {
-                    return (
-                        <MarkerF
-                            key={place.key}
-                            label={place.name} //Not super pretty, wanted to see if it works
-                            position={place.coord}
-                            icon={place.icon}
-                        ></MarkerF>
-                    );
-                })
-            }
-            <></>
-        </GoogleMap>
+        <div className="App">
+            <header>
+
+            </header>   
+                <nav className ="navbar">
+                    <li>
+                    <a href="/">Main</a>
+                    </li>
+                    <li>
+                    <a href="/">Latest Entries</a>
+                    </li>
+                    <li>
+                    <a href="/">The Team</a>
+                    </li>
+                </nav>
+                    <GoogleMap
+                    mapContainerStyle={containerStyle}
+                    center={center}
+                    zoom={zoom}
+                    onClick={handleMapClick}
+
+                    // onLoad={onLoad}
+                    // onUnmount={onUnmount}
+                >
+                    {infoWindow && (
+                    <InfoWindow position={infoWindow}>
+                    <form >     
+                    {/* onSubmit={handleSubmit} */}
+                    <div>New Entry at  ({infoWindow.lat}/{infoWindow.lng})</div><pre></pre>
+                    <label>
+                        What have you noticed?: <pre></pre>
+                        <form>
+                        <select value = {option}>
+                            <option value="lights">Lights Broken</option>
+                            <option value="traffic">Messy Traffic</option>
+                            <option value="pot">Potholes</option>
+                            <option value="construct">Dirty / Unaccessible Construction Zone</option>
+                            <option value="sidewalk">Small Sidewalk</option>
+                        </select>
+                        </form>
+                    </label>
+                    <pre></pre>
+                    <label>
+                        Briefly explain the issue: <pre></pre>
+                        <input className="descriptionfield" type="text" maxLength="100" value={description} />
+                    </label> <pre></pre>
+                    <input type="submit" value="Submit"  />
+                    </form>
+                    </InfoWindow>
+                    )}
+                </GoogleMap>
+        </div>
+        
     ) : (
-        <></>
+        <>Loading...</>
     );
 }
 
 export default App;
+
+
