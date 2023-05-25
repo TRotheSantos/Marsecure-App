0037ea4fad9580b99d8da5ea79023bba93193658
Yafet Suleman <yafet.suleman.ys@gmail.com>
2023-05-25 16:11:29 +0200

    wip

---
+++ front/src/entrieslist.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+class Entrieslist extends React.Component {
+    // fetch("/api/entries", {
+    //     method: "GET",
+    //     headers: { "Content-type": "application/json" },
+    // })
+    //     .then((result) => {
+    //         return result.json();
+    //     })
+    //     .then((data) => console.log(data));
+
+    render() {
+        return <h1>Hejsan</h1>;
+    }
+}
+
+export default Entrieslist;
